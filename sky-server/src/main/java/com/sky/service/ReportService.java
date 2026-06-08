d97dfb3c7fe166f59d771ff0414bc9eaa3c35f19
ailashi-126066 <sdr728328@gmail.com>
2026-06-08 15:46:26 +0800

    营业额统计功能

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,10 @@
+package com.sky.service;
+
+import java.time.LocalDate;
+
+import com.sky.vo.TurnoverReportVO;
+
+public interface ReportService
+{
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+}
